4ed4aa4b18483e5dda95abf7e99cbc7b5eafe773
jesaldkotak <101126798+jesaldkotak@users.noreply.github.com>
2026-04-01 16:47:07 +0530

    Change pcm_th function to use double instead of float

--- Arduino/pcm_th.h
+++ Arduino/pcm_th.h
@@ -2,5 +2,5 @@
 #ifndef PCM_TH_H
 #define PCM_TH_H
 // We pass cpt_out[] and cpt_count to store the results instead of returning a vector
-void pcm_th(float x[], int length_x, float thr_const, int s, int e, int points, int k_l, int k_r, int cpt_out[], int& cpt_count);
+void pcm_th(double x[], int length_x, double thr_const, int s, int e, int points, int k_l, int k_r, int cpt_out[], int& cpt_count);
 #endif
