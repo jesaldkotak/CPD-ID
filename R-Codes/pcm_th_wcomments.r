8e527e53a093733f53d54995c904d762c5434619
jesaldkotak <101126798+jesaldkotak@users.noreply.github.com>
2026-05-30 23:06:04 +0100

    Refactor pcm_th_wcomments function with logging
    Interval updation after CPD changed.

--- R-Codes/pcm_th_wcomments.r
+++ R-Codes/pcm_th_wcomments.r
@@ -5,9 +5,13 @@
 #  namespace:IDetect
 #with value
 
-function (x, sigma = stats::mad(diff(x)/sqrt(2)), thr_const = 1, thr_fin = sigma * thr_const * sqrt(2 * log(length(x))), s = 1, 
-e = length(x), points = 3, k_l = 1, k_r = 1) 
-{   #s:these conditions need not be checked in every iteration
+pcm_th_wcomments <- function (x, sigma = stats::mad(diff(x)/sqrt(2)), thr_const = 1, thr_fin = sigma * thr_const * sqrt(2 * log(length(x))), s = 1, 
+e = length(x), points = 3, k_l = 1, k_r = 1, log_file = 'cusum_val_2026_p10_t11_0cp_30.csv') 
+{   print(thr_fin)
+    print(sigma)
+    print(sqrt(2 * log(length(x))))
+    
+    #s:these conditions need not be checked in every iteration
     if (!(is.numeric(x))) {
         stop("The input in `x' should be a numeric vector containing the data in\n           which you would like to find change-points.")
     }
@@ -39,64 +43,122 @@ e = length(x), points = 3, k_l = 1, k_r = 1)
         lur <- length(left_points)
         rur <- length(right_points)
         #following two if loops make sure that after one CP is detected, the same intervals are not repeated
-        if (k_r < k_l) {                                        #a CP was detected in left half of plane, so start from right exp interval until size of right int = left int
-            while ((chp == 0) & (k_r < min(k_l, rur))) {        #no cp detected AND k_r < k_l, 
-                x_temp_r <- x[s:right_points[k_r]]              #creating a vector of points for the interval under test
-                ipcr <- cusum_function(x_temp_r)                #calculating cusum for a vector and storing in a vector
-                pos_r[k_r] <- which.max(abs(ipcr)) + s - 1      #argument of maxima of cusum stat in the interval   #what is there's no max?- unlikely due to noise
-                CUSUM_r[k_r] <- abs(ipcr[pos_r[k_r] - s + 1])   #absolute value of cusum stat at the maxima
-                if (CUSUM_r[k_r] > thr_fin) {                   #comparing with threshold
-                  chp <- pos_r[k_r]                             #assigning the arg with max cusum value only if it crosses threshold
-                }
-                else {
-                  k_r <- k_r + 1                                #until k_l = k_r
-                }
+        if (k_r < k_l) {                                        
+          while ((chp == 0) & (k_r < min(k_l, rur))) {        
+            x_temp_r <- x[s:right_points[k_r]]              
+            ipcr <- cusum_function(x_temp_r)                
+            pos_r[k_r] <- which.max(abs(ipcr)) + s - 1      
+            CUSUM_r[k_r] <- abs(ipcr[pos_r[k_r] - s + 1])   
+            
+            
+            if (CUSUM_r[k_r] > thr_fin) {                   
+              chp <- pos_r[k_r]                             
+              cp_order <<- cp_order + 1
             }
+            new_log_r <- data.frame(
+              Direction="Right", 
+              Exp_Interval=k_r, 
+              Start_Index=s,                      
+              End_Index=right_points[k_r],        
+              Candidate_Index=pos_r[k_r], 
+              Max_CUSUM=CUSUM_r[k_r],
+              Order=cp_order
+            )
+            write.table(new_log_r, log_file, append=TRUE, sep=",", col.names=FALSE, row.names=FALSE, quote=FALSE)
+            
+            if (CUSUM_r[k_r] <= thr_fin) {                   
+              k_r <- k_r + 1                                
+            }
+          }
         }
-        if (k_l < k_r) {                                        #a CP was detected in right half of plane, so start from left exp interval until size of right int = left int
-            while ((chp == 0) & (k_l < min(k_r, lur))) {        #similar to above
-                x_temp_l <- x[left_points[k_l]:e]               #IUT
-                ipcl <- cusum_function(x_temp_l)                #cusum calculation
-                pos_l[k_l] <- which.max(abs(ipcl)) + left_points[k_l] - 1
-                CUSUM_l[k_l] <- abs(ipcl[pos_l[k_l] - left_points[k_l] + 1])
-                if (CUSUM_l[k_l] > thr_fin) {
-                  chp <- pos_l[k_l]
-                }
-                else {
-                  k_l <- k_l + 1                                #until k_l = k_r
-                }
+        if (k_l < k_r) {                                        
+          while ((chp == 0) & (k_l < min(k_r, lur))) {        
+            x_temp_l <- x[left_points[k_l]:e]               
+            ipcl <- cusum_function(x_temp_l)                
+            pos_l[k_l] <- which.max(abs(ipcl)) + left_points[k_l] - 1
+            CUSUM_l[k_l] <- abs(ipcl[pos_l[k_l] - left_points[k_l] + 1])
+            
+            
+            if (CUSUM_l[k_l] > thr_fin) {
+              chp <- pos_l[k_l]
+              cp_order <<- cp_order + 1
             }
+            new_log_l <- data.frame(
+              Direction="Left", 
+              Exp_Interval=k_l, 
+              Start_Index=left_points[k_l],       
+              End_Index=e,                        
+              Candidate_Index=pos_l[k_l], 
+              Max_CUSUM=CUSUM_l[k_l],
+              Order=cp_order 
+            )
+            write.table(new_log_l, log_file, append=TRUE, sep=",", col.names=FALSE, row.names=FALSE, quote=FALSE)
+            
+            if (CUSUM_l[k_l] <= thr_fin) {
+              k_l <- k_l + 1                                
+            }
+          }
         }
-        if (chp == 0) {                                         #First the code comes here because k_l = k_r and no CPs detected yet
-            while ((chp == 0) & (k_l <= lur) & (k_r <= rur)) {  #initally and when k_l = k_r, No CP detected AND not finished running cusum on all the left and right intervals
-                x_temp_r <- x[s:right_points[k_r]]              #start with right interval
-                ipcr <- cusum_function(x_temp_r)                #cusum calc
-                pos_r[k_r] <- which.max(abs(ipcr)) + s - 1      #arg max
-                CUSUM_r[k_r] <- abs(ipcr[pos_r[k_r] - s + 1])   #absoulte
-                if (CUSUM_r[k_r] > thr_fin) {                   #compare to threshold
-                  chp <- pos_r[k_r]                             #add as a CP
-                }
-                else {                                          #if given point has value less than thresh in the right interval
-                  x_temp_l <- x[left_points[k_l]:e]             #if no CP, go to left interval
-                  ipcl <- cusum_function(x_temp_l)              #cusum cal
-                  pos_l[k_l] <- which.max(abs(ipcl)) + left_points[k_l] - 1
-                  CUSUM_l[k_l] <- abs(ipcl[pos_l[k_l] - left_points[k_l] + 1])
-                  if (CUSUM_l[k_l] > thr_fin) {                 #if greater than thresh in left interval
-                    chp <- pos_l[k_l]
-                  }
-                  else {                                        #increment both until a CP is found, or we tested all the left/right intervals
-                    k_r <- k_r + 1                              #If so, we won't enter the above two loops without user compulsion
-                    k_l <- k_l + 1
-                  }
-                }
+        if (chp == 0) {                                         
+          while ((chp == 0) & (k_l <= lur) & (k_r <= rur)) {  
+            x_temp_r <- x[s:right_points[k_r]]              
+            ipcr <- cusum_function(x_temp_r)                
+            pos_r[k_r] <- which.max(abs(ipcr)) + s - 1      
+            CUSUM_r[k_r] <- abs(ipcr[pos_r[k_r] - s + 1])   
+            
+            
+            if (CUSUM_r[k_r] > thr_fin) {                   
+              chp <- pos_r[k_r]                             
+              cp_order <<- cp_order + 1
             }
+            new_log_r <- data.frame(
+              Direction="Right", 
+              Exp_Interval=k_r, 
+              Start_Index=s,                      
+              End_Index=right_points[k_r],        
+              Candidate_Index=pos_r[k_r], 
+              Max_CUSUM=CUSUM_r[k_r],
+              Order=cp_order 
+            )
+            write.table(new_log_r, log_file, append=TRUE, sep=",", col.names=FALSE, row.names=FALSE, quote=FALSE)
+            
+            if (CUSUM_r[k_r] <= thr_fin) {                   
+              x_temp_l <- x[left_points[k_l]:e]             
+              ipcl <- cusum_function(x_temp_l)              
+              pos_l[k_l] <- which.max(abs(ipcl)) + left_points[k_l] - 1
+              CUSUM_l[k_l] <- abs(ipcl[pos_l[k_l] - left_points[k_l] + 1])
+            
+              
+              
+              if (CUSUM_l[k_l] > thr_fin) {                 
+                chp <- pos_l[k_l]
+                cp_order <<- cp_order + 1
+              }
+              new_log_l <- data.frame(
+                Direction="Left", 
+                Exp_Interval=k_l, 
+                Start_Index=left_points[k_l],       
+                End_Index=e,                        
+                Candidate_Index=pos_l[k_l], 
+                Max_CUSUM=CUSUM_l[k_l],
+                Order=cp_order 
+              )
+              write.table(new_log_l, log_file, append=TRUE, sep=",", col.names=FALSE, row.names=FALSE, quote=FALSE)
+              if (CUSUM_l[k_l] <= thr_fin) {                                        
+                k_r <- k_r + 1                              
+                k_l <- k_l + 1
+              }
+            }
+          }
         }
         if (chp != 0) {                                             #If a CP is detected
-            if (chp > ((e + s)/2)) {                                #if its in the right half plane
-                r <- pcm_th(x, s = s, e = chp, points = points, thr_fin = thr_fin, k_r = k_r, k_l = 1)  #running the next iteration in [s,chp], will start from left interval this time
+            #if (chp > ((e + s)/2)) {                                #if its in the right half plane
+            if ((chp - 1) - s >= points) {
+                r <- pcm_th_wcomments(x, s = s, e = chp, points = points, thr_fin = thr_fin, k_r = k_r, k_l = 1, log_file)  #running the next iteration in [s,chp], will start from left interval this time
             }
-            else {                                                  #if CP is in left half plane
-                r <- pcm_th(x, s = chp + 1, e = e, points = points, thr_fin = thr_fin, k_r = 1, k_l = max(1, k_l - 1)) #running the next iteration in [chp+1,e], will start from right interval
+            #else {                                                  #if CP is in left half plane
+            if (e - (chp + 1) >= points) {
+                r <- pcm_th_wcomments(x, s = chp + 1, e = e, points = points, thr_fin = thr_fin, k_r = 1, k_l = max(1, k_l - 1), log_file) #running the next iteration in [chp+1,e], will start from right interval
             }
             cpt <- c(chp, r)                                        #after a CP is detected, it is added to the cpt vector
         }
